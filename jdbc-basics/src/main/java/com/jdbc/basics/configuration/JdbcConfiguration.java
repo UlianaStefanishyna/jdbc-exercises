22ebe6c56c95e4c7911302ae0c3be6421687558f
Uliana Stefanishyna <ulanka1996@gmail.com>
2019-02-24 20:00:29 +0000

    added full steps

--- jdbc-basics/src/main/java/com/jdbc/basics/configuration/JdbcConfiguration.java
+++ jdbc-basics/src/main/java/com/jdbc/basics/configuration/JdbcConfiguration.java
@@ -1,12 +1,14 @@
 package com.jdbc.basics.configuration;
 
 import org.h2.jdbcx.JdbcDataSource;
+
 import javax.sql.DataSource;
 
 public class JdbcConfiguration {
 
     public static DataSource createInMemoryH2DatabaseDatasource(String databaseName, String username, String pass) {
         JdbcDataSource h2DataSource = new JdbcDataSource();
+
         String url = String.format("jdbc:h2:mem:%s;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=false", databaseName);
         h2DataSource.setUser(username);
         h2DataSource.setPassword(pass);
--- jdbc-basics/src/main/java/com/jdbc/basics/service/JdbcService.java
+++ jdbc-basics/src/main/java/com/jdbc/basics/service/JdbcService.java
@@ -14,19 +14,29 @@ public class JdbcService {
     private void initDatasource() {
         this.dataSource = JdbcConfiguration
                 .createInMemoryH2DatabaseDatasource(DATABASE_NAME, DATABASE_USER, DATABASE_USER_PASSWORD);
+        System.out.println(" > created datasource : " + dataSource);
     }
 
     private void createTable() throws SQLException {
+        System.out.println(" > opening connection");
         try (Connection connection = dataSource.getConnection()) {
+
+            System.out.println(" > statement creation");
             Statement statement = connection.createStatement();
+
+            System.out.println(" > executing (execute) sql query to create table");
             statement.execute(CREATE_TABLE_SQL);
         }
     }
 
     private void insertDataIntoTable() throws SQLException {
+        System.out.println(" > opening connection");
         try (Connection connection = dataSource.getConnection();) {
+
+            System.out.println(" > prepared statement creation");
             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_SQL);
 
+            System.out.println(" > executing (executeUpdate) sql query to insert data");
             preparedStatement.setString(1, "Test");
             preparedStatement.executeUpdate();
 
@@ -36,23 +46,37 @@ public class JdbcService {
     }
 
     private void printResult() throws SQLException {
+        System.out.println(" > opening connection");
         try (Connection connection = dataSource.getConnection()) {
+
+            System.out.println(" > statement creation");
             Statement statement = connection.createStatement();
+
+            System.out.println(" > executing (executeQuery) sql query to select data");
             ResultSet resultSet = statement.executeQuery(SELECT_ALL_SQL);
+
             while (resultSet.next()) {
-                System.out.println(resultSet);
                 String messageText = resultSet.getString(1);
                 Timestamp timestamp = resultSet.getTimestamp(2);
-                System.out.println(" - " + messageText + " {" + timestamp + "}");
+                System.out.println(" >> " + messageText + " {" + timestamp + "}");
             }
         }
     }
 
     public static void main(String[] args) throws SQLException {
+
         JdbcService jdbcService = new JdbcService();
+
+        System.out.println("\n-- 1. database initialization: create datasource");
         jdbcService.initDatasource();
+
+        System.out.println("\n-- 2. create table");
         jdbcService.createTable();
+
+        System.out.println("\n-- 3. insertion data");
         jdbcService.insertDataIntoTable();
+
+        System.out.println("\n-- 4. printing result");
         jdbcService.printResult();
     }
 }
